363ca0a5d0f15fd8fdbcab95996115567f999057
John, Didi <Didicjohn@gmail.com>
2021-01-27 22:51:05 +0530

    Cart with checkout

--- Cart.cs
+++ Cart.cs
@@ -1,13 +1,27 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace PromotionEngine
 {
     public class Cart
     {
+        Dictionary<string, int> _cartItemsQty;//ProductXQuantity
+        Dictionary<string, decimal> _productValues;//ProductXUnitPrice
+        public Dictionary<string, int> CartItemsQty { get => _cartItemsQty; set => _cartItemsQty = value; }
+        public Dictionary<string, decimal> ProductValues { get => _productValues; set => _productValues = value; }
+        public Cart()
+        {
+            CartItemsQty = new Dictionary<string, int>();
+            ProductValues = new Dictionary<string, decimal>();
+        }
 
         public decimal Checkout()
         {
-            return 0;
+            decimal billAmount = 0;
+
+            foreach (var item in _cartItemsQty)
+                billAmount += (item.Value * _productValues[item.Key]); //Qty*Price
+            return billAmount;
         }
     }
 }
